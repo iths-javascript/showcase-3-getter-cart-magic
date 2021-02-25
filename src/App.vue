57a9350434fd01af68b0e58e6d329c88d2662ed5
David Lundholm <david.lundholm@zocom.se>
2021-02-25 13:42:09 +0100

    Getter magic

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="products">
+      <h1>Products</h1>
+      <ul>
+        <li v-for="product in products" :key="product" @click="add(product.id)">{{product.title}}</li>
+      </ul>
+
+    </section>
+    <section class="state-cart">
+      <h1>State Cart</h1>
+      <ul>
+        <li v-for="cartItem in stateCart" :key="cartItem.productID">{{cartItem}}</li>
+      </ul>
+    </section>
+    <section class="getter-cart">
+      <h1>Getter Cart</h1>
+      <ul>
+        <li v-for="cartItem in getterCart" :key="cartItem.productID">{{cartItem}}</li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  created(){
+    this.$store.commit('LOAD_PRODUCTS')
+  },
+  computed: {
+    products(){ return this.$store.state.productList},
+
+    getterCart(){
+      return this.$store.getters.cart
+    },
+    stateCart(){
+      return this.$store.state.cart
+    },
+  },
+  methods: {
+    add(productID){
+      this.$store.commit('ADD_PRODUCT_TO_CART', productID)
+    }
   }
 }
 </script>
@@ -24,5 +56,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  min-height: 100vh;
 }
+
+ul{ list-style: none }
+li{
+  padding: 1rem;
+}
+li:nth-of-type(2n){
+  background-color: lightgrey;
+}
+.products li{
+  cursor: pointer;
+}  
+.products li:hover{ background-color: grey}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    productList: [],
+    products: {},
+
+    cart: [
+      //{productID: 37, amount: 1},
+    ]
   },
   mutations: {
+    LOAD_PRODUCTS(state){      
+      state.products = {
+        34: {id: 34, title: "Tjofläsk", price: 10},
+        37: {id: 37, title: "Tjoflöjt", price: 11},
+        137: {id: 137, title: "Tjoflöj1312t", price: 16},
+        3437: {id: 3437, title: "Tjoflöjt3213213", price: 12},
+      }
+      state.productList = Object.values(state.products)
+    },
+    ADD_PRODUCT_TO_CART(state, productID){
+      const cartItem = state.cart.find(cartItem => cartItem.productID == productID)
+      if(cartItem){ cartItem.amount++ }
+      else{ state.cart.push({productID, amount: 1})}
+    }
   },
-  actions: {
-  },
-  modules: {
+
+  getters: {
+    cart: state => state.cart.map( cartItem => ({
+      ...state.products[cartItem.productID],
+      amount: cartItem.amount
+    })),
   }
+
 })
